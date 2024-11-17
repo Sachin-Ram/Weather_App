78ec491686c9f57bd90519d573d6274f5ba07e1e
sachin <sachinram2510@gmail.com>
2024-11-17 20:30:05 +0530

    security bug fix

--- services/weather_services.py
+++ services/weather_services.py
@@ -29,8 +29,9 @@ class weather_services:
 
         longitude=lat_long_data[0]["lat"]
 
-        # weather_data=requests.get("https://api.openweathermap.org/data/2.5/weather?q=${}&units=metric&appid=d82786d494223277eaffb2a4feb1db4b".format(self.city,self.api_key))
+        
         weather_data=requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(self.city,self.api_key))
+        
         result=weather_data.json()
 
         return result
